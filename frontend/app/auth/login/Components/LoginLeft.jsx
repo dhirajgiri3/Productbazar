055cc37881c08f22e6489e65d1d31bcd8c61b1f3
dhirajgiri3 <2021356392.giri@ug.sharda.ac.in>
2025-05-25 17:53:59 +0530

    Login and register page fixes

--- frontend/app/auth/login/Components/LoginLeft.jsx
+++ frontend/app/auth/login/Components/LoginLeft.jsx
@@ -290,7 +290,7 @@ const LoginLeft = () => {
                     d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                   />
                 </svg>
-                <span className="font-medium">{error.message}</span>
+                <span className="font-medium">{error}</span>
                 {error.includes("no password") && (
                   <button
                     type="button"
